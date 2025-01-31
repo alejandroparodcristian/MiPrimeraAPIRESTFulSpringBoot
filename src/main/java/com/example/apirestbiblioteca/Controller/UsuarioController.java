b80e4f76affacf3b985f19c11b2a2073162e8d3c
alejandroparodcristian <115542447+alejandroparodcristian@users.noreply.github.com>
2025-01-31 10:45:35 +0100

    Validacion dni

--- src/main/java/com/example/apirestbiblioteca/Controller/UsuarioController.java
+++ src/main/java/com/example/apirestbiblioteca/Controller/UsuarioController.java
@@ -18,6 +18,43 @@ public class UsuarioController {
     UsuarioRepository usuarioRepository;
     public UsuarioController() {
     }
+
+    public boolean ValidarDNI(String dni) {
+        String[] letras = dni.split("");
+        String[] dni_letras = {"T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"};
+        if (letras.length != 9){
+            return false;
+        }
+        if (Character.isLetter(letras[0].charAt(0))){
+            int numeros = Integer.parseInt(dni.substring(1, 8));
+            if (letras[0].equals("X")) {
+                if (letras[8].equals(dni_letras[numeros % 23])){
+                    return true;
+                } else
+                    return false;
+            } else if (letras[0].equals("Y")) {
+                String numeros_actualizado = "1" + String.valueOf(numeros);
+                if (letras[8].equals(dni_letras[Integer.parseInt(numeros_actualizado) % 23])){
+                    return true;
+                } else
+                    return false;
+            } else if (letras[0].equals("Z")) {
+                String numeros_actualizado = "2" + String.valueOf(numeros);
+                if (letras[8].equals(dni_letras[Integer.parseInt(numeros_actualizado) % 23])){
+                    return true;
+                } else
+                    return false;
+            } else
+                return false;
+        } else {
+            int numeros = Integer.parseInt(dni.substring(0, 8));
+            if (letras[8].equals(dni_letras[numeros % 23])) {
+                return true;
+            } else
+                return false;
+            }
+    }
+
     @Autowired
     public UsuarioController(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
@@ -35,25 +72,34 @@ public class UsuarioController {
     }
 
     @PostMapping("/usuario")
-    public ResponseEntity<Usuario> add(@Valid @RequestBody Usuario usuario) {
-        Usuario u = this.usuarioRepository.save(usuario);
-        return ResponseEntity.ok(u);
+    public ResponseEntity<Object> add(@Valid @RequestBody Usuario usuario) {
+        if(ValidarDNI(usuario.getDni())){
+            Usuario u = this.usuarioRepository.save(usuario);
+            return ResponseEntity.ok(u);
+        }else {
+            return ResponseEntity.badRequest().body("El dni no es válido");
+        }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Usuario> update(@PathVariable int id ,@Valid @RequestBody Usuario usuario) {
+    public ResponseEntity<Object> update(@PathVariable int id ,@Valid @RequestBody Usuario usuario) {
         //buscamos el usuario en la bd
-        Usuario usuarioBD = this.usuarioRepository.findById(id).get();
-        //Lo guardamos y le hacemos los cambios nuevos
-        Usuario usuarioActualizado = usuarioBD;
-        usuarioActualizado.setNombre(usuario.getNombre());
-        usuarioActualizado.setTipo(usuario.getTipo());
-        usuarioActualizado.setEmail(usuario.getEmail());
-        usuarioActualizado.setPassword(usuario.getPassword());
-        usuarioActualizado.setPenalizacionHasta(usuario.getPenalizacionHasta());
-        //Guardamos los cambios en la bd y lo enseñamos
-        Usuario persist = this.usuarioRepository.save(usuarioActualizado);
-        return ResponseEntity.ok().body(persist);
+        if(ValidarDNI(usuario.getDni())){
+            Usuario usuarioBD = this.usuarioRepository.findById(id).get();
+            //Lo guardamos y le hacemos los cambios nuevos
+            Usuario usuarioActualizado = usuarioBD;
+            usuarioActualizado.setNombre(usuario.getNombre());
+            usuarioActualizado.setTipo(usuario.getTipo());
+            usuarioActualizado.setEmail(usuario.getEmail());
+            usuarioActualizado.setPassword(usuario.getPassword());
+            usuarioActualizado.setPenalizacionHasta(usuario.getPenalizacionHasta());
+            //Guardamos los cambios en la bd y lo enseñamos
+            Usuario persist = this.usuarioRepository.save(usuarioActualizado);
+            return ResponseEntity.ok().body(persist);
+        }else {
+            return ResponseEntity.badRequest().body("El dni no es válido");
+        }
+
     }
 
     @DeleteMapping("/borrar/")
